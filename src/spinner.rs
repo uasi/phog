c06a30d39c30b8fdafc0223a27751d6118371777
Tomoki Aonuma <uasi@uasi.jp>
2022-09-11 02:12:48 +0900

    Update indicatif to 0.17.0

--- src/spinner.rs
+++ src/spinner.rs
@@ -1,11 +1,14 @@
 use indicatif::{ProgressBar, ProgressStyle};
 
+use std::time::Duration;
+
 pub fn new_spinner(msg: String) -> ProgressBar {
     let style = ProgressStyle::default_spinner()
         .tick_strings(&["", ".", "..", "...", "....", ".....", "... Done."])
-        .template("{msg}{spinner}");
+        .template("{msg}{spinner}")
+        .expect("Failed to create spinner");
     let spinner = ProgressBar::new(1).with_style(style);
     spinner.set_message(msg);
-    spinner.enable_steady_tick(160);
+    spinner.enable_steady_tick(Duration::from_millis(160));
     spinner
 }
